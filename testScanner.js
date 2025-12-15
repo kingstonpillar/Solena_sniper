de13ec9c072a64d4f3c8b801ccbc852b1627364c
kingstonpillar <kingstonpillar@googlemail.com>
2025-12-15 09:58:23 +0100

    Delete testScanner.js

--- testScanner.js
+++
@@ -1,7 +0,0 @@
-import { verifyCreatorSafety } from "./tokenCreatorScanner.js";
-
-(async () => {
-  const mint = "So11111111111111111111111111111111111111112";
-  const result = await verifyCreatorSafety(mint);
-  console.log(JSON.stringify(result, null, 2));
-})();
