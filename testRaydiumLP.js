e0025eda1a5ec20968bf966c110b7005891b0c47
kingstonpillar <kingstonpillar@googlemail.com>
2025-12-16 16:36:08 +0100

    Delete testRaydiumLP.js

--- testRaydiumLP.js
+++
@@ -1,6 +0,0 @@
-import { testNewRaydiumLP } from "./newRaydiumToken.js";
-
-(async () => {
-  const pools = await testNewRaydiumLP(10000); // listen for 10 seconds
-  console.log("Detected LPs:", pools);
-})();
